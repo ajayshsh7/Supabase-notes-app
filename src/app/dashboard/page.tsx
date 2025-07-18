a238cc912b1cc848797d7539dfdd7729305e6e55
ajayshsh7 <ajayshsh7@gmail.com>
2025-07-18 17:37:12 +0530

    removing color tags for now

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -180,17 +180,6 @@ const handleTogglePin = async (noteId: number, currentStatus: boolean) => {
         rows={5}
         className={styles.textarea}
       />
-      <div className={styles.colourPicker}>
-  <label>Select Color Label:</label>
-  <select value={colour} onChange={(e) => setColour(e.target.value)} className={styles.select}>
-    <option value="gray">Gray</option>
-    <option value="red">Red</option>
-    <option value="blue">Blue</option>
-    <option value="green">Green</option>
-    <option value="yellow">Yellow</option>
-    <option value="purple">Purple</option>
-  </select>
-</div>
 
       <button onClick={handleCreateNote} className={styles.button}>
         Create Note
@@ -233,7 +222,9 @@ const handleTogglePin = async (noteId: number, currentStatus: boolean) => {
                 ) : (
                   <>
                     <div className={styles.noteHeader}>
-                      <h3>{note.title}</h3>
+                      <h3><span style={{
+
+                      }}></span>{note.title}</h3>
                       <div className={styles.actions}>
                         <span
                           className={styles.icon}
@@ -254,16 +245,14 @@ const handleTogglePin = async (noteId: number, currentStatus: boolean) => {
                           onClick={() => handleDeleteNote(note.id)}
                           title="Delete"
                         >
-                          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
-    <path d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z"></path>
-</svg>
+                          🗑️
                         </span>
                       </div>
                     </div>
-                    <p>{note.content}</p>
                   </>
                 )}
               </div>
+                <p>{note.content}</p>
             </li>
           ))}
         </ul>
@@ -323,10 +312,10 @@ const handleTogglePin = async (noteId: number, currentStatus: boolean) => {
                         </span>
                       </div>
                     </div>
-                    <p>{note.content}</p>
                   </>
                 )}
               </div>
+                <p>{note.content}</p>
             </li>
           ))}
         </ul>
